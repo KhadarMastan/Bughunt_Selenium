fa0888f55a3cafbf51a899e1cb3ada2573f2935a
Khadar Mastan Shaik <khadarmastan2011@gmail.com>
2025-07-27 15:44:58 +0530

    initial commit

---
+++ Test002.java
@@ -0,0 +1,11 @@
+package org.BugHunter.Khadar;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing");
+
+	}
+
+}
